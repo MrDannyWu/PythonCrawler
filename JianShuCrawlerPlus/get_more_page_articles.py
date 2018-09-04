f5c2df06c08f73dc92a664b5428cc9de7ae59533
DannyWu <loveloveyouareheart365@gmail.com>
2018-09-04 16:22:27 +0800

    Create get_more_page_articles.py

---
+++ JianShuCrawlerPlus/get_more_page_articles.py
@@ -0,0 +1,44 @@
+'''
+author：DannyWu
+site:   www.idannywu.com
+'''
+
+import requests
+#from bs4 import BeautifulSoup
+import json
+from selenium import webdriver
+import time
+from get_article_contents import get_article_content
+#import pandas
+#import PhantomJS
+
+url = 'https://www.jianshu.com'
+
+def get_one_page_article(one_page_url):
+    #do = webdriver.ChromeOptions()
+    #do.add_argument(r'user-data-dir=C:\Users\DannyWu\AppData\Local\Google\Chrome\User Data')
+   # browser = webdriver.Chrome("chromedriver", 0, do)
+    browser = webdriver.Chrome()
+    browser.get(one_page_url)
+    articles = []
+    for i in range(4):
+        browser.execute_script("window.scrollTo(0, document.body.scrollHeight);") 
+        time.sleep(2)    
+    for j in range(2):  
+        try:
+            button = browser.execute_script("var a = document.getElementsByClassName('load-more'); a[0].click();")
+            time.sleep(2)
+        except:
+            pass
+
+        titles = browser.find_elements_by_class_name("title")
+        for title in titles[:len(titles) - 2]:
+
+            if title == 'None':
+                pass
+            else:
+                print(title.get_attribute('href'))
+                article_url = title.get_attribute('href')
+                get_article_content(article_url)
+ 
+get_one_page_article(url)
