1e756e4e4749951ae8fe9135a70002ae90fa49d8
DannyWu <loveloveyouareheart365@gmail.com>
2018-09-11 22:18:27 +0800

    Update get_more_page_articles.py

--- JianShuCrawler/get_more_page_articles.py
+++ JianShuCrawler/get_more_page_articles.py
@@ -8,8 +8,9 @@ import requests
 import json
 from selenium import webdriver
 import time
-from get_article_content import get_article_content
-import pandas
+from get_article_contents import get_article_content
+#import pandas
+#import PhantomJS
 
 url = 'https://www.jianshu.com'
 
@@ -17,8 +18,9 @@ def get_one_page_article(one_page_url):
     do = webdriver.ChromeOptions()
     do.add_argument(r'user-data-dir=C:\Users\DannyWu\AppData\Local\Google\Chrome\User Data')
     browser = webdriver.Chrome("chromedriver", 0, do)
+    #browser = webdriver.Chrome()
     browser.get(one_page_url)
-    articles = []
+    #articles = []
     for i in range(4):
         browser.execute_script("window.scrollTo(0, document.body.scrollHeight);") 
         time.sleep(2)    
@@ -28,15 +30,15 @@ def get_one_page_article(one_page_url):
             time.sleep(2)
         except:
             pass
+
     titles = browser.find_elements_by_class_name("title")
     for title in titles[:len(titles) - 2]:
+
         if title == 'None':
             pass
         else:
             print(title.get_attribute('href'))
             article_url = title.get_attribute('href')
-            articles.append(get_article_content(article_url))
-    df = pandas.DataFrame(articles)
-    df.to_excel('jbook.xlsx')
-
+            get_article_content(article_url)
+ 
 get_one_page_article(url)
