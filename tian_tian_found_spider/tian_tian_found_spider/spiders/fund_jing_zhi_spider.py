584ed791db0819e56cae9b036374516b47d83015
WUZHIQIANGX <loveloveyouareheart365@gmail.com>
2020-08-02 23:45:17 +0800

    添加抓取天天基金站点估值、净值板块的基金基本信息

---
+++ tian_tian_found_spider/tian_tian_found_spider/spiders/fund_jing_zhi_spider.py
@@ -0,0 +1,38 @@
+import scrapy
+import json
+from tian_tian_found_spider.items import TianTianFoundSpiderItem
+
+
+class FundJingZhiSpiderSpider(scrapy.Spider):
+    """
+    抓取天天基金净值
+    """
+    custom_settings = {
+
+        'DEFAULT_REQUEST_HEADERS': {
+            'Connection': 'keep-alive',
+            # 'Cookie': 'st_si=40529082525104; st_asi=delete; st_pvi=51871271130091; st_sp=2020-08-02%2018%3A55%3A50; st_inirUrl=https%3A%2F%2Fwww.baidu.com%2Flink; st_sn=10; st_psi=20200802201439758-0-7839806187',
+            'DNT': '1',
+            'Host': 'fund.eastmoney.com',
+            'Referer': 'http://fund.eastmoney.com',
+        }
+    }
+    name = 'fund_jing_zhi_spider'
+    allowed_domains = ['fund.eastmoney.com']
+    start_urls = ['http://fund.eastmoney.com/Data/Fund_JJJZ_Data.aspx?t=1&lx=1&letter=&gsid=&text=&sort=zdf,desc&page={},200&dt=1596373971516&atfc=&onlySale=0'.format(i) for i in range(1, 47)]
+
+    def parse(self, response):
+        # print(response.text)
+        # tr_list = response.xpath('//tbody[@id="tableContent"]/tr')
+        # for i in tr_list:
+        #     print(i)
+        # print(response.text.split('datas:')[1].split(',count:')[0])
+        json_data = json.loads(response.text.split('datas:')[1].split(',count:')[0], encoding='utf-8')
+        print(json_data)
+        for i in json_data:
+            item = TianTianFoundSpiderItem()
+            data = {}
+            for j, k in zip(i, range(50)):
+                data['field{}'.format(k)] = j
+            item = data
+            yield item
