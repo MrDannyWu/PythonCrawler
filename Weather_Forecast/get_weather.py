b4332ba2dd62f884fd8420c3998d098db5f7a9ef
danny <danny@kreditme.id>
2020-08-04 09:36:29 +0800

    修改天气抓取，修改网络签名

--- Weather_Forecast/get_weather.py
+++ Weather_Forecast/get_weather.py
@@ -1,30 +1,30 @@
+'''
+author : DannyWu
+site   : www.idannywu.com
+'''
 import requests
-import selenium
-from selenium import webdriver as wb
 from bs4 import BeautifulSoup
-from cities import cities
+from citylist import cities
 from prettytable import PrettyTable
+import json
+from db import DB_Utils
+
+header = {
+        'Cookie':'vjuids=4438bee9b.166809f026c.0.e55a83d533b4b; vjlast=1539756196.1539756196.30; Hm_lvt_080dabacb001ad3dc8b9b9049b36d43b=1539756197; Hm_lpvt_080dabacb001ad3dc8b9b9049b36d43b=1539756197; userNewsPort0=1; f_city=%E5%85%AD%E5%AE%89%7C101221501%7C',
+        'user-agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.92 Safari/537.36'
+        }
+
 def pretty_print(trains,header):
-    #trains=[[11,12,13,14,15,16],[21,22,23,24,25,26],[31,32,33,34,35,36]]
-    #trains = [[12,12,12,1212,2],[1,2,3,4,6],[6,5,4,3,7]]
-    #header = [colum1,colum2,colum3,colum4,colum5]
-    #print(header)
     pt=PrettyTable()
     pt._set_field_names(header)
     for train in trains:
         pt.add_row(train)
     print(pt)
-#pretty_print('list1','list2','list3','list4','list5')
 
-
-#url = 'http://www.weather.com.cn/weather1d/101220101.shtml'
-#url = 'http://www.weather.com.cn/weather1d/101161206.shtml'
 def get_city_weather_url(city_name):
     base_url = 'http://www.weather.com.cn/weather1d/{}.shtml'
     for key in cities:
-        if key in city_name:
-            city_name = key
-        elif city_name in key:
+        if key == city_name:
             city_name = key
     print(city_name)
     try:
@@ -33,77 +33,66 @@ def get_city_weather_url(city_name):
         print(url)
         return url
     except:
-        print('你输入的城市不存在！')
-
-header = {
-        'Cookie':'vjuids=4438bee9b.166809f026c.0.e55a83d533b4b; vjlast=1539756196.1539756196.30; Hm_lvt_080dabacb001ad3dc8b9b9049b36d43b=1539756197; Hm_lpvt_080dabacb001ad3dc8b9b9049b36d43b=1539756197; userNewsPort0=1; f_city=%E5%85%AD%E5%AE%89%7C101221501%7C',
-        'user-agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.92 Safari/537.36'
-        }
-def get_weather_details(url):
-    web_data = get_weather_html(url)
-    #print(web_data)
-    try:
-        html = BeautifulSoup(web_data,'lxml')
-        address = html.select('.ctop .crumbs a')
-        abc = html.select('.ctop .crumbs')
-        address1 = html.select('.ctop .crumbs span')
-        times = html.select('#curve .time em')
-        wpics = html.select('#curve .wpic div big')
-        tems = html.select('#curve .tem em')
-        winfs = html.select('#curve .winf em')
-        winls = html.select('#curve .winl em')
-        #print(abc)
-        #print(times)
-
-#        for time in times:
-#            print(time.text)
-#        for wpic in wpics:
-#            print(wpic.get('title'))
-#        for tem in tems:
-#            print(tem.text)
-#        for winf in winfs:
-#            print(winf.text)
-#        for winl in winls:
-#            print(winl.text)
-        #print(html)
-    except:
-        print('无内容...')
-    list1 = []
-    list2 = []
-    #print(address[0].text,address[1].text,address1[-1].text+' 今日天气：')
-    #print(address.text)
-#    for i in address:
-#        print(i.text,end=' ')
-#    for j in address1:
-#        print(j.text,end=' ')
- #   print(' 今日天气：')
-
-    print('')
-    print(abc[0].text.replace('\n','').replace(' ',''),'实时天气：')
-    for time,wpic,tem,winf,winl in zip(times,wpics,tems,winfs,winls):
-        #print(time.text,wpic.text,tem.text,winf.text,winl.text,end='\n')
-        list1 = [time.text,wpic.get('title'),tem.text,winf.text,winl.text]
-        
-        list2.append(list1)
-    #print(list1)
-    #for i in list2:
-    #    print(i)
-    header = ['时间','天气','温度','风向','风级']
-    pretty_print(list2,header)
-
-    #print(curve)
+        print('此城市城市不存在！')
 
 def get_weather_html(url):
-    browser = wb.PhantomJS()
     try:
-        browser.get(url)
-        web_data = browser.page_source
-        return web_data
+        web_data = requests.get(url)
+        web_data.encoding = 'UTF-8'
+
+        return web_data.text
     except:
         print('请求页面失败...')
 
 
-#get_weather_html(url)
-key = input("亲输入城市名称：")
-url = get_city_weather_url(key)
-get_weather_details(url)
+def get_1day_weather_data(web_data):
+    soup = BeautifulSoup(web_data, 'lxml')
+    datas = soup.select('script')
+    abc = soup.select('.ctop .crumbs')
+    db_utils = DB_Utils()
+    for data in datas:
+        if 'hour3data' in str(data):
+            data1 = {}
+            data1 = data.text.split('=')[-1]
+            # print(data1)
+            data2 = json.loads(data1)
+            # print(data2['1d'])
+            print(abc[0].text.replace('\n', '').replace(' ', ''), '实时天气：')
+            city = abc[0].text.replace('\n', '').replace(' ', '')
+            city_info = city + '实时天气：\n'
+            data4 = []
+            for i in data2['1d']:
+                # print(i.split(','))
+                #print(i.split(',')[0], end=' ')
+                # print(i.split(',')[1])
+                #print(i.split(',')[2], end=' ')
+                #print(i.split(',')[3], end=' ')
+                #print(i.split(',')[4], end=' ')
+                #print(i.split(',')[5])
+                data3 = [i.split(',')[0],i.split(',')[2] ,i.split(',')[3],i.split(',')[4],i.split(',')[5]]
+                print(data3)
+                data4.append(data3)
+            weather_data = {
+                    'city':city,
+                    '24hour_weather':data4
+                    }
+            db_utils.save_one_to_mongo('24hour_weather',weather_data)
+            print('save to mongo')
+
+
+if __name__ == "__main__":
+    db_util = DB_Utils()
+    db_util.drop_collection('24hour_weather')
+    web_data_list = []
+    for key in cities:
+        url = get_city_weather_url(key)
+        web_data = get_weather_html(url)
+        get_1day_weather_data(web_data)
+        #web_data_list.append(web_data)
+
+    #get_weather_data(web_data)
+    #pool = Pool(1)
+    #pool.map(get_1day_weather_data,web_data_list)
+    #pool.close()
+
+
--- sign/mysign2.py
+++ sign/mysign2.py
@@ -5,27 +5,24 @@ def create_sign(name):
     url = 'http://www.jiqie.com/a/re19.php'
     form_data = {
         'id': name,
-        'idi': 'jiqie',
-        'id1': '1002',
-        'id2': '20',
+        'zhenbi': '20191123',
+        'id1': '1605',
+        'id2': '363',
         'id3':'#000000',
-        'id4':'#000000',
-        'id5':'2',
-        'id6':'',
+        'id4':'#FFF7D1',
+        'id5':  -1
         }
     web_data = requests.post(url,data=form_data)
     web_data.encoding = 'utf-8'
     soup = BeautifulSoup(web_data.text,'lxml')
-    #print(soup)
-
-    tu = "http://www.jiqie.com" + soup.select('img')[0].get('src').split('.')[2] + "." + soup.select('img')[0].get('src').split('.')[3]
-    #print(tu)
+    tu = soup.select('img')[0].get('src')
     img_res = requests.get(tu)
     with open(name+'.jpg','wb') as f:
         f.write(img_res.content)
-create_sign("DannyWu")
+        f.close()
+
 def main():
-    name = input("请输入您的名字：")
+    name = input("请输入您的名字(最好不超过四个汉字)：")
     create_sign(name)
     print("您的签名已完成，以图片的形式保存在当前目录！")
 
